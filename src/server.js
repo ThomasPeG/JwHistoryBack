bfd8d0fd5bacc23891515235229fb72e8a66e5dc
Thomas Perez <tomapego16@gmail.com>
2025-05-12 14:11:00 -0400

    cambio test

--- src/server.js
+++ src/server.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => {
 // Puerto del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puertos ${PORT}`);
 });
